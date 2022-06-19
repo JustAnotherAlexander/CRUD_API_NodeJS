229a9272792e3ad3bbbb6ee540e7d52f1353f099
Aleksandr <aleksandreliuz@gmail.com>
2022-06-19 23:16:28 +0300

    feat: add post functionality

--- api.js
+++ api.js
@@ -1,16 +1,45 @@
 import http from 'http';
+import {v4 as uuidv4} from 'uuid';
 
 const server = http.createServer((request, response) => {
     let arr = [];
-    console.log(request)
-    if (request.url === '/api/users' && request.method === 'GET') {
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'application/json');
-        response.end(JSON.stringify(arr));
+    let msg = 'Please send data in the correct format!'
+
+    if (request.method === 'GET') {
+        console.log(request.url)
+        if (request.url === '/api/users') {
+            console.log(request.url);
+            response.statusCode = 200;
+            response.setHeader('Content-Type', 'application/json');
+            response.end(JSON.stringify(arr));
+        }
     }
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'application/json');
-    response.end();
+
+    
+
+    if (request.url === '/api/users' && request.method === 'POST') {
+        let body = '';
+        request.on('data', (chunk) => {
+            body += chunk.toString();
+            console.log(body);
+        })
+
+        request.on('end', () => {
+            const test = JSON.parse(body);
+            if (test.username !== '' && test.age !== '' && test.hobbies.length>0) {
+                let user = {};
+                user = {id: uuidv4(), ...test};
+                arr.push(user);
+                response.statusCode = 201;
+                response.setHeader('Content-Type', 'application/json');
+                response.end(JSON.stringify(arr));
+            } else {
+                response.statusCode = 400;
+                response.setHeader('Content-Type', 'application/json');
+                response.end(JSON.stringify(msg));
+            }
+        })
+    }
 });
 
 const port = process.env.port || '4000';
