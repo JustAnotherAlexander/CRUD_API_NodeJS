e526e553b5cce6bc858d245e302bc27fa28ae734
Aleksandr <aleksandreliuz@gmail.com>
2022-06-19 23:59:40 +0300

    feat: add GET functionality to find items

--- api.js
+++ api.js
@@ -7,11 +7,41 @@ const server = http.createServer((request, response) => {
 
     if (request.method === 'GET') {
         console.log(request.url)
-        if (request.url === '/api/users') {
+        let test = request.url.split('/');
+        console.log(test)
+        if (request.url === '/api/users' && test.length === 3) {
             console.log(request.url);
+            console.log(test)
             response.statusCode = 200;
             response.setHeader('Content-Type', 'application/json');
             response.end(JSON.stringify(arr));
+        } else if (request.url === '/api/users' && test.length > 3) {
+            console.log(test[3].length)
+            if (test[3].split('').length < 36) {
+                response.statusCode = 400;
+                response.setHeader('Content-Type', 'application/json');
+                response.end(JSON.stringify(msg));
+            } else {
+                let foundItem = {};
+                arr.forEach((item)=>{
+                    if (item.id === test[2]) {
+                        foundItem = item; 
+                    }
+                });
+                if (foundItem.length!=0) {
+                    response.statusCode = 200;
+                        response.setHeader('Content-Type', 'application/json');
+                        response.end(JSON.stringify(founditem));
+                } else {
+                    response.statusCode = 404;
+                    response.setHeader('Content-Type', 'application/json');
+                    response.end(JSON.stringify("Item not found"));
+                }
+            }
+        } else {
+            response.statusCode = 400;
+            response.setHeader('Content-Type', 'application/json');
+            response.end(JSON.stringify(msg));
         }
     }
 
